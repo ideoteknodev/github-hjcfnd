b27209b25d0d43eebec9d6d2405a0c5b74dd0631
ideoteknodev <85555949+ideoteknodev@users.noreply.github.com>
2023-07-19 20:56:01 +0700

    2 files modified

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,13 @@
 <template>
-  <section>
-    <p>This page will be displayed at the /about route.</p>
-  </section>
+  <div>
+    <h1>About to the homepage</h1>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+    <AppAlert> This is an auto-imported component </AppAlert>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h1>Welcome to the homepagesdf</h1>
+    <h1>Welcome to the homepage</h1>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
     <AppAlert> This is an auto-imported component </AppAlert>
   </div>
 </template>
